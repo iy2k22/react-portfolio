5e1f5fc61bc4a2ffa71c9197f87fca2c23993415
Ibrahim Yusuf <iy830755@gmail.com>
2024-01-29 16:00:34 +0000

    add contact

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => (
   <div id="app">
     <Header />
     <Projects />
+    <Contact />
   </div>
-)
+);
 
 export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,8 @@
+const Contact = () => (
+    <ul id="contact">
+        <li key="github"><a href="https://github.com/iy2k22">GitHub</a></li>
+        <li key="email"><a href="mailto:ibrahimyusufdev@gmail.com">E-mail</a></li>
+    </ul>
+);
+
+export default Contact;
